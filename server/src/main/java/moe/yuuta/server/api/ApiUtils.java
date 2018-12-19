010a80ae76e80274aa12f5f9681596fe6039f3ff
YuutaW <17158086+trumeet@users.noreply.github.com>
2018-12-18 18:14:36 -0800

    fix(server): remove uncatched IOExceptions
    Signed-off-by: Trumeet <17158086+Trumeet@users.noreply.github.com>

--- server/src/main/java/moe/yuuta/server/api/ApiUtils.java
+++ server/src/main/java/moe/yuuta/server/api/ApiUtils.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 public class ApiUtils {
     public static String objectToJson (Object object) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(object);
@@ -17,11 +19,11 @@ public class ApiUtils {
         }
     }
 
-    public static <V> V jsonToObject (String json, Class<V> t) {
+    public static <V> V jsonToObject (String json, Class<V> t) throws IOException {
         return new ObjectMapper().readValue(json, t);
     }
 
-    public static <V> V jsonToObject (String json, TypeReference<V> t) {
+    public static <V> V jsonToObject (String json, TypeReference<V> t) throws IOException {
         return new ObjectMapper().readValue(json, t);
     }
 }
