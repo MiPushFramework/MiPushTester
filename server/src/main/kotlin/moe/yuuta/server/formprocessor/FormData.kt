7c82f4371ea8ad8ef80cfee26d0042646e9c723c
Trumeet <17158086+trumeet@users.noreply.github.com>
2020-08-16 20:37:09 -0700

    fix(server): ignore null strings as well when ignorable=true
    It seems like the MiPush Server had an update sometimes before which started returning errors if 'extra.jobkey=' is present. This parameter was left blank intentionally but was not ignored before.
    
    Signed-off-by: Trumeet <17158086+Trumeet@users.noreply.github.com>

--- server/src/main/kotlin/moe/yuuta/server/formprocessor/FormData.kt
+++ server/src/main/kotlin/moe/yuuta/server/formprocessor/FormData.kt
@@ -5,4 +5,7 @@ package moe.yuuta.server.formprocessor
 @Retention(AnnotationRetention.RUNTIME)
 annotation class FormData(val name: String,
                           val urlEncode: Boolean = false,
+                          // If it is true, the fields with default values (String: "", Number: 0) will be removed.
+                          // If it is false, the fields with default values will be kept as 'key=' or 'key=0', etc.
+                          // Nulls are always removed.
                           val ignorable: Boolean = true)
--- server/src/test/java/moe/yuuta/server/formprocessor/HttpFormTest.java
+++ server/src/test/java/moe/yuuta/server/formprocessor/HttpFormTest.java
@@ -10,7 +10,9 @@ public class HttpFormTest {
     public void shouldToBuffer() {
         assertEquals("normalString=haoye&" +
                         "normalInteger=123&" +
-                        "encodeString=+++Ri+kk+a",
+                        "encodeString=+++Ri+kk+a&" +
+                        "shouldNotIgnored=&" +
+                        "shouldNotIgnored2=0",
                 HttpForm.toBuffer(new SampleObject()).toString().trim());
         // Give the ignorable integer a value so it won't be ignored
         SampleObject sampleObject = new SampleObject();
@@ -18,7 +20,9 @@ public class HttpFormTest {
         assertEquals("normalString=haoye&" +
                         "normalInteger=123&" +
                         "encodeString=+++Ri+kk+a&" +
-                        "ignorableInteger=2333",
+                        "ignorableInteger=2333&" +
+                        "shouldNotIgnored=&" +
+                        "shouldNotIgnored2=0",
                 HttpForm.toBuffer(sampleObject).toString().trim());
     }
 }
